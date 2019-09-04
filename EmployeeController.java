5c15abfc94f6d9461062a3c6c17969696eb53d2d
ArnabXX2 <40687806+ArnabXX2@users.noreply.github.com>
2019-09-04 11:48:19 +0530

    Add files via upload

--- EmployeeController.java
+++ EmployeeController.java
@@ -10,6 +10,8 @@ import com.rohit.demo.EmployeeRepository;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 
 @RestController
 @RequestMapping("/api/vi")
@@ -26,8 +28,27 @@ public class EmployeeController {
 	@GetMapping("/employees/{id}")
 	public ResponseEntity<Employee> getEmployeesById(@PathVariable(value="id")Long employeeId){
 	//throws ResourceNotFoundException{
-		Employee employee=employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee not found on :: "+employeeId));
+		Employee employee=employeeRepository.findById(employeeId);//.orElseThrow(() -> new ResourceNotFoundException("Employee not found on :: "+employeeId));
 		return ResponseEntity.ok().body(employee);
 	}
+	
+	@PostMapping("/employees")
+	public Employee createEmployee(@Valid @RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+	@PutMapping("employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable(value="id") Long employeeId,@Valid @RequestBody Employee employeeDetails){
+		Employee employee=employeeRepository.findById(employeeId);
+		
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setDesignation(employeeDetails.getDesignation());
+		final Employee updatedEmployee=employeeRepository.save(employee);
+		return ResponseEntity.ok(updatedEmployee);
+	}
+	
+	
+	
 	
 }
